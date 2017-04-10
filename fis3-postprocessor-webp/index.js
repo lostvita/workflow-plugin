436f08651e6b571cd7c1ecaec3f1534146cd36c2
丘东伟 <wion@gm99.com>
2017-04-10 15:15:41 +0800

    修复bug：对scss中的图片转化为webp图片

--- fis3-postprocessor-webp/index.js
+++ fis3-postprocessor-webp/index.js
@@ -1,100 +1,100 @@
 let fs          = require('fs');
 
 let cheerio     = require('cheerio'),
-	exec        = require('child_process').exec;
+    exec        = require('child_process').exec;
 
 const utils = {
-	state:{
-		regxHtml: /^\.html/i,
-		regxScss: /^\.css/i,
-		regxWebp: /\?__webp$/i
-	},
+    state:{
+        regxHtml: /^\.html/i,
+        regxScss: /^\.css/i,
+        regxWebp: /\?__webp$/i
+    },
 
-	isHtml: function(str){
-		return this.state.regxHtml.test(str);
-	},
+    isHtml: function(str){
+        return this.state.regxHtml.test(str);
+    },
 
-	isScss: function(str){
-		return this.state.regxScss.test(str);
-	},
+    isScss: function(str){
+        return this.state.regxScss.test(str);
+    },
 
-	isWebp: function(str){
-		return this.state.regxWebp.test(str);
-	},
+    isWebp: function(str){
+        return this.state.regxWebp.test(str);
+    },
 
-	webper: function(settings, fileSrc, dest){
-		let args = ['-quiet', '-mt'];
+    webper: function(settings, fileSrc, dest){
+        let args = ['-quiet', '-mt'];
 
-		if (settings.preset) {
-		    args.push('-preset', settings.preset);
-		}
+        if (settings.preset) {
+            args.push('-preset', settings.preset);
+        }
 
-		if (settings.quality) {
-		    args.push('-q', settings.quality);
-		}
+        if (settings.quality) {
+            args.push('-q', settings.quality);
+        }
 
-		if (settings.alphaQuality) {
-		    args.push('-alpha_q', settings.alphaQuality);
-		}
+        if (settings.alphaQuality) {
+            args.push('-alpha_q', settings.alphaQuality);
+        }
 
-		if (settings.method) {
-		    args.push('-m', settings.method);
-		}
+        if (settings.method) {
+            args.push('-m', settings.method);
+        }
 
-		if (settings.size) {
-		    args.push('-size', settings.size);
-		}
+        if (settings.size) {
+            args.push('-size', settings.size);
+        }
 
-		if (settings.sns) {
-		    args.push('-sns', settings.sns);
-		}
+        if (settings.sns) {
+            args.push('-sns', settings.sns);
+        }
 
-		if (settings.filter) {
-		    args.push('-f', settings.filter);
-		}
+        if (settings.filter) {
+            args.push('-f', settings.filter);
+        }
 
-		if (settings.autoFilter) {
-		    args.push('-af');
-		}
+        if (settings.autoFilter) {
+            args.push('-af');
+        }
 
-		if (settings.sharpness) {
-		    args.push('-sharpness', settings.sharpness);
-		}
+        if (settings.sharpness) {
+            args.push('-sharpness', settings.sharpness);
+        }
 
-		if (settings.lossless) {
-		    args.push('-lossless');
-		}
+        if (settings.lossless) {
+            args.push('-lossless');
+        }
 
-		exec('cwebp ' + args.concat([fileSrc, '-o', dest]).join(' '), function(err) {
-	        if (err) throw err;
-	    });
-	},
+        exec('cwebp ' + args.concat([fileSrc, '-o', dest]).join(' '), function(err) {
+            if (err) throw err;
+        });
+    },
 
-	rewriteHtml: function($){
-		let imgs = $('img'),
-		len = imgs.length;
-	}
+    rewriteHtml: function($){
+        let imgs = $('img'),
+        len = imgs.length;
+    }
 }
 
 // 解析html模板
 const parseHtml = function(content, file, settings){
-	let _root = file.dirname.split('/static')[0];
-	let ctn = content,
-		$ = cheerio.load(ctn),
-		imgs = $('img'),
-		len = imgs.length,
-		isRewrite = false;
+    let _root = file.dirname.split('/static')[0];
+    let ctn = content,
+        $ = cheerio.load(ctn),
+        imgs = $('img'),
+        len = imgs.length,
+        isRewrite = false;
 
-	for(let i=0; i<len; i++){
+    for(let i=0; i<len; i++){
         let img = $(imgs[i]),
-        	src = img.attr('src'),
-        	fullsrc = utils.isWebp(src) ? (_root + src.split('?__webp')[0]) : null;
+            src = img.attr('src'),
+            fullsrc = utils.isWebp(src) ? (_root + src.split('?__webp')[0]) : null;
         if(fullsrc && fs.existsSync(fullsrc)){
-        	isRewrite = true;
-        	let dest = fullsrc + ".webp";
-        	utils.webper(settings,fullsrc,dest);
+            isRewrite = true;
+            let dest = fullsrc + ".webp";
+            utils.webper(settings,fullsrc,dest);
 
-        	let alt = img.prop('alt') || '',
+            let alt = img.prop('alt') || '',
                 title = img.prop('title') || '',
                 name = src.split('?__webp')[0],
                 html = "<picture><source srcset=\"" + name + ".webp\" type=\"image/webp\"><img srcset=\"" + name + "\" src=\"" + name + "\" alt=\"" + alt + "\" title=\"" + title + "\"></picture>"
@@ -103,8 +103,8 @@ const parseHtml = function(content, file, settings){
         }
     }
     if(isRewrite){
-    	let headEl = $('head'),
-    		webpScript = `<script>;(function(doc) {
+        let headEl = $('head'),
+            webpScript = `<script>;(function(doc) {
                 // 给html根节点加上webps类名
                 function addRootTag() {
                     doc.documentElement.className += " webps";
@@ -127,19 +127,19 @@ const parseHtml = function(content, file, settings){
                     addRootTag();
                 }
             }(document));</script>`;
-    	headEl.append(webpScript);
-    	ctn = $.html();
+        headEl.append(webpScript);
+        ctn = $.html();
     }
     return ctn;
 }
 
 const parseCss = function(content, file, settings){
-	let html = '',
+    let html = '',
         ctn = content.toString("utf8");  // buffer转utf8;
 
     // 已经注入过webps类，先要剔除
     let rmStart = ctn.indexOf('.webps'),
-    	isRemove = rmStart != '-1' && ctn.indexOf('.webp') != -1;  // 后一个判断表示图片后缀
+        isRemove = rmStart != '-1' && ctn.indexOf('.webp') != -1;  // 后一个判断表示图片后缀
     if(isRemove){
         ctn = ctn.slice(0,rmStart);
     }
@@ -147,7 +147,7 @@ const parseCss = function(content, file, settings){
     ctn.replace(/url\([\'\"](\S+)[\'\"]\)/gi,function(str,src,index){
         src = src.split(')')[0];
         let _root = file.dirname.split('/static')[0],
-        	name = src.slice(0,src.lastIndexOf('?')),
+            name = src.slice(0,src.lastIndexOf('?')),
             start = ctn.lastIndexOf('{',index),
             end = ctn.indexOf('}',start) + 1,
             styleCtn = ctn.slice(start,end),
@@ -169,7 +169,10 @@ const parseCss = function(content, file, settings){
 
         // 生成webp图片
         let fullsrc = utils.isWebp(src) ? (_root + src.split('?__webp')[0]) : null;
-        utils.webper(settings,fullsrc,dest);
+        if(fullsrc){
+            let dest = fullsrc + ".webp";
+            utils.webper(settings,fullsrc,dest);
+        }
 
         let clss = '';
         for(let i=0,l=clsses.length; i<l; i++){
@@ -209,11 +212,11 @@ const parseCss = function(content, file, settings){
 }
 
 module.exports = function (content, file, settings) {
-	let ctn = content;
-	if(utils.isHtml(file.rExt)){
-		ctn = parseHtml(content, file, settings);
-	}else if(utils.isScss(file.rExt)){
-		ctn = parseCss(content, file, settings);
-	}
-	return ctn;
+    let ctn = content;
+    if(utils.isHtml(file.rExt)){
+        ctn = parseHtml(content, file, settings);
+    }else if(utils.isScss(file.rExt)){
+        ctn = parseCss(content, file, settings);
+    }
+    return ctn;
 }
