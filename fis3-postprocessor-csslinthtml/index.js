8ca3b7e23d088c2f1ce3ebd855e4daba3ddac3cc
丘东伟 <wion@gm99.com>
2017-04-20 17:24:32 +0800

    【ADD】添加基于css3检测html代码的fis3插件

---
+++ fis3-postprocessor-csslinthtml/index.js
@@ -0,0 +1,56 @@
+let cheerio     = require('cheerio'),
+    he          = require('he');
+
+const utils = {
+    state:{
+        regxHtml: /^\.html/i,
+        lintMap:{
+        	"errors": "//events.gm99.com/static/scss/common/csslint/errors.css",
+        	"warns": "//events.gm99.com/static/scss/common/csslint/warns.css",
+        	"advices": "//events.gm99.com/static/scss/common/csslint/advices.css"
+        }
+    },
+
+    isHtml: function(str){
+        return this.state.regxHtml.test(str);
+    },
+
+    linter: function(settings){
+        let rst = '',
+        	types = settings.type,
+        	lints = settings.lint || [];
+
+        for(let i=0,l=types.length; i<l; i++){
+        	if(types[i]){
+        		let url = lints[i] ? lints[i] : utils.state.lintMap[types[i]];
+        		rst += url ? "<link rel=\"stylesheet\" type=\"text/css\" href=\""+ url +"\">" : '';
+        	}
+        }
+
+        return rst;
+    }
+}
+
+// 解析html模板
+const parseHtml = function(content, file, settings){
+    let _root = file.dirname.split('/static')[0];
+    let ctn = content,
+        $ = cheerio.load(ctn),
+        headEl = $('head');
+
+    let css = utils.linter(settings);
+    headEl.append(css);
+
+    ctn = he.decode($.html());
+    return ctn;
+}
+
+
+module.exports = function (content, file, settings) {
+	let ctn = content;
+
+    if(settings.useCssLint && utils.isHtml(file.rExt)){
+        ctn = parseHtml(content, file, settings);
+    }
+    return ctn;
+}
---
+++ fis3-postprocessor-csslinthtml/readme.md
@@ -0,0 +1,41 @@
+### Fis3-postprocessor-csslinthtml
+
+fis3-postprocessor-csslinthtml插件是通过css高级选择器去校验html代码的语义化和规范化，目前该插件支持3种规范程度的校验。分别为：errors、warns和advices。
+
+### Install
+
+```
+npm install fis3-postprocessor-csslinthtml --save-dev
+```
+
+说明，该插件依赖于cheerio和he模块，安装插件的同时需要安装这两个模块：
+
+```
+npm install cheerio he --save-dev
+```
+
+### Usage
+
+```javascript
+fis.match('/html/**/*.html', {
+    postprocessor:[fis.plugin('csslinthtml',{
+        useCssLint: true,
+        type:["errors"], // 类型：errors、warns、advices
+        lint:["www.example.com/static/css/csslint/errors.css"] // css检测规则样式表
+      })
+    ]
+});
+```
+
+- useCssLint ：设置为false则关闭该功能
+- type：设置检测的类型，目前支持errors、warns和advices
+- lint：css检测样式表，插件默认为该三种检测类型映射了检测样式表，用户可以不再设置。如果用户想要使用自己的检测规则，则在这里填写样式表的超链接。
+
+> 注意：type跟lint的位置要互相匹配
+
+### Role
+
+> 这里的检测规则引用的是[a11y.css](http://ffoodd.github.io/a11y.css/index.html)，同时推荐html检测的chrome插件[可以检查html代码的chrome扩展](https://zhuanlan.zhihu.com/p/26268863)
+
+
+
